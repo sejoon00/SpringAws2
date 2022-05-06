1e1ecfacc4284e60227347586e23698236b9de60
sejoon_00 <parksejoon313@gmail.com>
2022-05-07 00:35:20 +0900

    Ch2

---
+++ src/test/java/com/sejoon/book/springboot/web/HelloControllerTest.java
@@ -0,0 +1,54 @@
+package com.sejoon.book.springboot.web;
+
+import junit.framework.TestCase;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.regex.Matcher;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest( controllers = HelloController.class)
+public class HelloControllerTest {
+
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello리턴() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto리턴() throws Exception {
+        String name = "hello";
+        int amount = 10000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+
+
+}
